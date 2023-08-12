d5186c5eafa8b3981677cc545a2f7a03ce28a1e1
davidthai4 <85862746+davidthai4@users.noreply.github.com>
2023-08-11 23:22:24 -0700

    Delete davidthai4.github.io directory

--- davidthai4.github.io/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const themeSwitcher = document.getElementById("theme-switcher");
-    const body = document.body;A
-    themeSwitcher.addEventListener("click", function() {
-        body.classList.toggle("dark-theme");
-    })
-})
